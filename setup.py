b12136cf339c82a71746768285fa13af456f03bc
sirrkitt <33910211+sirrkitt@users.noreply.github.com>
2018-10-20 15:13:04 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
   name='radicale_ldap3_auth',
   version='0.0.1',
   description='Simple, rough, LDAP3 auth plugin for Radicale',
-  long_description=open('README.txt').read(),
+  long_description=open('README').read(),
   license='LICENSE',
   author='Jacob Louis Lemus Peschel',
   author_email='jacob@tlacuache.us',
-  packages=['radicale_ldap3_auth'],
-  install_requires['ldap3', 'radicale'],
+  install_requires=['radicale', 'ldap3'],
+  packages=['radicale_ldap3_auth']
 )
