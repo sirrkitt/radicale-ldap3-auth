0134ef06f0a65fc50da212744bc15e2781b8f6d5
sirrkitt <33910211+sirrkitt@users.noreply.github.com>
2018-10-20 14:58:00 -0700

    Update setup.py
    Fixed formatting and added stuff to setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,14 @@
 
 from distutils.core import setup
 
-setup(name="radicale_ldap3_auth", packages=["radicale_ldap3_auth"])
+setup(
+  name='radicale_ldap3_auth',
+  version='0.0.1',
+  description='Simple, rough, LDAP3 auth plugin for Radicale',
+  long_description=open('README.txt').read(),
+  license='LICENSE.txt',
+  author='Jacob Louis Lemus Peschel',
+  author_email='jacob@tlacuache.us',
+  packages=['radicale_ldap3_auth'],
+  install_requires['ldap3', 'radicale'],
+)
